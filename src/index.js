f5d5a8a57a678108db5acc3d83b8c2db614c0923
Rishabh Singh <rishabdahiya98@gmail.com>
2023-03-07 18:00:33 +0530

    updated auth to latest versiono

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// aQwJzDKRp9fRwF7gVUMsOQT2X9reKj2N
-// dev-3eurzvjel1u8wk0v.us.auth0.com
 
 root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-    redirectUri={window.location.origin}
-    cacheLocation="localstorage"
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
   >
     <UserProvider>
       <ProductsProvider>
